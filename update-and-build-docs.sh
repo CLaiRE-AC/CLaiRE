7fa5b2d2999e6020330a8ed22fca028988432dcd
netuoso <andrew@boonedevelopment.com>
2025-03-08 16:33:28 -0600

    Add automated CNAME creation to docs builder

--- update-and-build-docs.sh
+++ update-and-build-docs.sh
@@ -10,4 +10,12 @@ echo "Checking in changed files"
 git add docs/ site/
 echo "Committing updated documentation"
 git commit -m "Rebuilt CLaiRE documentation"
+echo "Entering main directory"
+cd ..
+echo "Adding CNAME to gh-pages"
+git checkout gh-pages
+echo "docs.claire.ac" > CNAME
+git add CNAME
+git commit -m "CNAME"
+git checkout main
 echo "Finished updating, building, and deploying documentation"
